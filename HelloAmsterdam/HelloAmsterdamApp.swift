4c48f8f07d8293b81be28e79899803e15da4cfb0
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-08-26 12:09:47 -0400

    Initial Commit

---
+++ HelloAmsterdam/HelloAmsterdamApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAmsterdamApp.swift
+//  HelloAmsterdam
+//
+//  Created by Lukas Nguyen on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct HelloAmsterdamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
