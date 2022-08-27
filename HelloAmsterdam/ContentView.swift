20136c1ac2addb765843d5b7b1c2b8a20b666c60
Anh-Toine <52642269+Anh-Toine@users.noreply.github.com>
2022-08-27 11:38:52 -0400

    First commit

--- HelloAmsterdam/ContentView.swift
+++ HelloAmsterdam/ContentView.swift
@@ -11,15 +11,16 @@ struct ContentView: View {
     var body: some View {
 		ZStack{
 			
-			Image("Amsterdam")
-				.resizable()
-				.aspectRatio(contentMode: .fit)
-				.frame(width: 1000, height: 1000)
-			
+//			Image("Amsterdam")
+//				.resizable()
+//				.aspectRatio(contentMode: .fit)
+//				.frame(width: 1000, height: 1000)
+//
 			Text("Hello Amsterdam!")
 				.fontWeight(.bold)
 				.font(.title)
 				.foregroundColor(Color.white)
+				.background(Image("amsterdam-1"))
 				.shadow(color: Color.black, radius: 4.23, x: 0.6, y: 0.6)
 
 				.padding()
